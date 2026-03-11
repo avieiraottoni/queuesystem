e0cf8beed24b2fca813214d2202bfba7ff195386
andre.ottoni <avieiraottoni@gmail.com>
2026-03-11 17:14:45 -0300

    002

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
 
 Route::get('/', function () {
-    echo "Queue System";
+    // testar a conexão com a base de dados
+
+    try {
+
+        DB::connection()->getPdo();
+        echo 'Conexão, ok!';
+        
+    } catch (Exception $e) {
+        echo 'Não foi possível conectar com o banco de dados! Erro: '. $e->getMessage();
+    }
 });
