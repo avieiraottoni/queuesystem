d19dedb839b74ee0726e77fa950ee6f2c536a99d
andre.ottoni <avieiraottoni@gmail.com>
2026-03-12 17:44:58 -0300

    Criação do layout do frontend

---
+++ resources/views/components/layouts/guest-layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name')}} {!! empty($subtitle) ? '': ' &vellip; ' . $subtitle !!}</title>
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" type="image/png">
+    {{-- load resources | fontawesome 6.x --}}
+    <link rel="stylesheet" href="{{ asset('assets/fontawesome/css/all.min.css') }}">
+    @vite('resources/css/app.css')
+</head>
+<body>
+    
+    {{ $slot }}
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     
-    echo config('app.name');
-
+    return view('teste', ['subtitle' => 'Home']);
 });
+
