7d99172c172f8ef43452bd1a89215e18064d3b68
andre.ottoni <avieiraottoni@gmail.com>
2026-03-13 14:46:43 -0300

    Formulários de validação de login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,11 +7,34 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     public function login() {
+        
+        // Verifica se existem erros de validação de formulário
+        if(session('errors')) {
+            dd(session('errors')->all(), old());
+        }
+        
         return view('auth.login_frm');
     }
 
     public function loginSubmit(Request $request) {
-        echo "Login submit!!!";
+       // form validation
+        $request->validate(
+            // regras de validação    
+            [
+                'username'  => 'required|email',
+                'password'  => 'required|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,16}$/'
+            ],
+            // Mensagens de erro
+            [
+                'username.required' => 'O usuário é obrigatório.',
+                'username.email'    => 'O usuário deve ser um e-mail válido.',
+                'password.required' => 'A senha é obrigatória.',
+                'password.regex'    => 'A senha deve conter entre 6 e 16 caracteres, ter ao menos uma letra maiúscula, uma minúscula e um dígito.' 
+            ]
+        );
+
+        echo 'Ok';
+
     }
 
     public function logout() {
